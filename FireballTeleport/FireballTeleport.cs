643be13b987abcb2c0cb83cb0015fe898457fb58
Eren <e.basaran2004@gmail.com>
2025-02-07 14:33:33 +0100

    fix: made patch run before the actual method

--- FireballTeleport/FireballTeleport.cs
+++ FireballTeleport/FireballTeleport.cs
@@ -30,61 +30,16 @@ public class FireballTeleport : BaseUnityPlugin
             .Select(rb => rb.gameObject)
             .ToList();
         GameObject player = players[0];
-        
-        // Access private members using reflection
-        FieldInfo projectileTypeField = typeof(Wish.Projectile).GetField("_projectileType", BindingFlags.NonPublic | BindingFlags.Instance);
-        FieldInfo pierceCountField = typeof(Wish.Projectile).GetField("_pierceCount", BindingFlags.NonPublic | BindingFlags.Instance);
-        FieldInfo numPiercesField = typeof(Wish.Projectile).GetField("_numPierces", BindingFlags.NonPublic | BindingFlags.Instance);
-        FieldInfo hasExplodeAnimationField = typeof(Wish.Projectile).GetField("hasExplodeAnimation", BindingFlags.NonPublic | BindingFlags.Instance);
-        FieldInfo delayField = typeof(Wish.Projectile).GetField("delay", BindingFlags.NonPublic | BindingFlags.Instance);
-        FieldInfo animField = typeof(Wish.Projectile).GetField("anim", BindingFlags.NonPublic | BindingFlags.Instance);
-            
-        // Access the current values of the private members
-        ProjectileType projectileType = (Wish.ProjectileType)projectileTypeField.GetValue(__instance);
-        int pierceCount = (int)pierceCountField.GetValue(__instance);
-        int numPierces = (int)numPiercesField.GetValue(__instance);
-        bool hasExplodeAnimation = (bool)hasExplodeAnimationField.GetValue(__instance);
-        float delay = (float)delayField.GetValue(__instance);
-        Animator anim = (Animator)animField.GetValue(__instance);
 
+        // Reference fireball
         GameObject gameObject = __instance.gameObject;
         
-        switch (projectileType)
+        if (gameObject.name == "Fireball(Clone)")
         {
-            case ProjectileType.Piercing:
-                pierceCount++;
-                pierceCountField.SetValue(__instance, pierceCount);
-                if (pierceCount > numPierces)
-                {
-                    Destroy(gameObject);
-                    return false; 
-                }
-                break;
-
-            case ProjectileType.InfinitePierce:
-                break;
-
-            default:
-                // Teleport player to the fireball position
-                if (gameObject.name == "Fireball(Clone)")
-                {
-                    log.LogInfo("Teleporting player to fireball position!");
-                    player.transform.position = gameObject.transform.position;
-                }
-                    
-                if (hasExplodeAnimation)
-                {
-                    anim.Play("Explosion");
-                    DOVirtual.DelayedCall(delay, () => Object.Destroy(gameObject));
-                }
-                else
-                {
-                    Destroy(gameObject);
-                }
-                break;
+            log.LogInfo("Teleporting player to fireball position!");
+            player.transform.position = gameObject.transform.position;
         }
         
-        return false; 
-        
+        return true;
     }
 }
